720f16faef68ec9fca05586a2081819bd90f2ff0
wangweipeng <weipeng.wang@gllue.com>
2018-10-30 20:45:49 +0800

    update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton
 from quamash import QEventLoop
 from scanhost import manager
 import asyncio
+import pyperclip
 
 
 loop = QEventLoop(QApplication(sys.argv))
@@ -13,21 +14,28 @@ ui = mainwindow.Ui_MainWindow()
 ui.setupUi(MainWindow)
 MainWindow.show()
 
-QPushButton(ui.topFiller).setText("fre")
-
 label_count = 0
+
+def get_text_from_editor():
+    return ui.textEdit.toPlainText()
+
+def get_button(data):
+    btn = QPushButton(ui.topFiller)
+    btn.setText(data)
+    btn.clicked.connect(lambda x: pyperclip.copy(data))
+    return btn
 
 def on_reg(data):
     return 'success'
 
 def on_text(data):
     global label_count
-    btn = QPushButton(ui.topFiller)
+    btn = get_button(data)
     btn.setText(data)
     btn.move(0, label_count * 40)
     btn.show()
     label_count += 1
-    return f'一段文本{data}'
+    return f'success'
 
 
 def listen():
@@ -35,6 +43,16 @@ def listen():
     manager.reg_msg_handler('text', on_text)
     manager.listen()
 
+def send_msg():
+    data = get_text_from_editor()
+    ui.textEdit.clear()
+    global label_count
+    btn = get_button(data)
+    btn.move(384, label_count * 40)
+    btn.show()
+    label_count += 1
+
+ui.submit.clicked.connect(send_msg)
 listen()
 
 # manager.heartbeat()
